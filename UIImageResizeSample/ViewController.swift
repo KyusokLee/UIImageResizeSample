5de3f7d96b9aa8f3d462cf722d235bb62de49fbd
KyusokLee <kyusok1@naver.com>
2024-12-14 10:42:58 +0900

    [Add] - add button and imageView

--- UIImageResizeSample/ViewController.swift
+++ UIImageResizeSample/ViewController.swift
@@ -2,18 +2,67 @@
 //  ViewController.swift
 //  UIImageResizeSample
 //
-//  Created by gyusoku.i on 2024/12/14.
+//  Created by Kyusok.lee on 2024/12/14.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var firstImageView: UIImageView = {
+        let imageView = UIImageView()
+
+        return imageView
+    }()
+
+    private lazy var secondImageView: UIImageView = {
+        let imageView = UIImageView()
+
+        return imageView
+    }()
+
+    private lazy var firstButton: UIButton = {
+        let button = UIButton()
+
+        button.addAction(.init { [weak self] _ in
+            guard let self else { return }
+            self.firstButtonTapped()
+        }, for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var secondButton: UIButton = {
+        let button = UIButton()
+
+        button.addAction(.init { [weak self] _ in
+            guard let self else { return }
+            self.secondButtonTapped()
+        }, for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.addSubview(firstImageView)
+//        view.addSubview(secondImageView)
+//        view.addSubview(firstButton)
+//        view.addSubview(secondButton)
+        setupUI()
+    }
+
+    private func setupUI() {
+
     }
 
 
+
+    private func firstButtonTapped() {
+
+    }
+
+    private func secondButtonTapped() {
+
+    }
 }
 
