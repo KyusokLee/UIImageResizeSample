8e911553e417d90408b9b7f04c6699b9da8d6c29
KyusokLee <kyusok1@naver.com>
2024-12-14 10:26:57 +0900

    Initial Commit

---
+++ UIImageResizeSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIImageResizeSample
+//
+//  Created by gyusoku.i on 2024/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
